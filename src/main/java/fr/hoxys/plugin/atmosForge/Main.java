2cd298e97ce36ee0d4a6ae42d2c59cbabb827e48
Lauric Hoxys <lauricsomeil@icloud.com>
2025-04-13 17:32:48 +0200

    initiale

---
+++ src/main/java/fr/hoxys/plugin/atmosForge/Main.java
@@ -0,0 +1,17 @@
+package fr.hoxys.plugin.atmosForge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
